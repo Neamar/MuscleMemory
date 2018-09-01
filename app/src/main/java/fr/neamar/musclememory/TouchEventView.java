86028d076e2d17f42493418140221c8de68b8dff
Neamar <neamar@neamar.fr>
2018-09-01 10:10:07 +0100

    Basic touch detection

---
+++ app/src/main/java/fr/neamar/musclememory/TouchEventView.java
@@ -0,0 +1,97 @@
+package fr.neamar.musclememory;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PointF;
+import android.util.AttributeSet;
+import android.util.SparseArray;
+import android.view.MotionEvent;
+import android.view.View;
+
+
+public class TouchEventView extends View {
+
+    private static final int SIZE = 90;
+
+    private SparseArray<PointF> mActivePointers;
+    private Paint mPaint;
+
+    private Paint textPaint;
+
+
+    public TouchEventView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initView();
+    }
+
+    private void initView() {
+        mActivePointers = new SparseArray<>();
+        mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+
+        mPaint.setColor(Color.BLUE);
+        mPaint.setStyle(Paint.Style.FILL_AND_STROKE);
+        textPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        textPaint.setTextSize(20);
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        // get masked (not specific to a pointer) action
+        int maskedAction = event.getActionMasked();
+
+        switch (maskedAction) {
+            case MotionEvent.ACTION_DOWN:
+            case MotionEvent.ACTION_POINTER_DOWN: {
+                // We have a new pointer. Lets add it to the list of pointers
+                int pointerIndex = event.getActionIndex();
+                int pointerId = event.getPointerId(pointerIndex);
+
+                PointF f = new PointF();
+                f.x = event.getX(pointerIndex);
+                f.y = event.getY(pointerIndex);
+                mActivePointers.put(pointerId, f);
+                break;
+            }
+            case MotionEvent.ACTION_MOVE: { // a pointer was moved
+                for (int size = event.getPointerCount(), i = 0; i < size; i++) {
+                    PointF point = mActivePointers.get(event.getPointerId(i));
+                    if (point != null) {
+                        point.x = event.getX(i);
+                        point.y = event.getY(i);
+                    }
+                }
+                break;
+            }
+            case MotionEvent.ACTION_UP:
+            case MotionEvent.ACTION_POINTER_UP:
+            case MotionEvent.ACTION_CANCEL: {
+                int pointerId = event.getPointerId(event.getActionIndex());
+
+                mActivePointers.remove(pointerId);
+                break;
+            }
+        }
+        invalidate();
+
+        return true;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        // draw all pointers
+        for (int size = mActivePointers.size(), i = 0; i < size; i++) {
+            PointF point = mActivePointers.valueAt(i);
+            if (point != null) {
+                mPaint.setColor(Color.BLUE);
+                canvas.drawCircle(point.x, point.y, SIZE, mPaint);
+            }
+        }
+        canvas.drawText("Total pointers: " + mActivePointers.size(), 10, 40, textPaint);
+    }
+}
